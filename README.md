a54690b9efb9de8720f35ed54f33eb9cedae9742
Hannes Oswald <hannes.oswald97@gmail.com>
2024-11-20 20:41:23 +0100

    Translate two-way

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-* Translate two-way
+# README
+
+FastTranslate is a Browser extension that facilitates quick translations. It automatically translates in both ways.
+
+## Feature Backlog
+
+* Replace text when context menu entry clicked
 * Configure languages
 * Support manifest v2 or v3?
 * Chrome Support
--- background.js
+++ background.js
@@ -6,6 +6,18 @@ browser.menus.create({
   contexts: ["selection"]
 });
 
+browser.menus.onShown.addListener(async (info, tab) => {
+  const selectedText = info.selectionText.trim();
+  const translatedWord1 = await fetchTranslationBing(selectedText, "de");
+
+  let displayedText = translatedWord1;
+  if(equalsIgnoringCase(selectedText, translatedWord1)){
+    displayedText = await fetchTranslationBing(selectedText, "en");
+  }
+
+  updateMenuItem(displayedText);
+});
+
 function updateMenuItem(text) {
   browser.menus.update(MENU_ITEM_ID, {
     title: text
@@ -13,22 +25,17 @@ function updateMenuItem(text) {
   browser.menus.refresh();
 }
 
-browser.menus.onShown.addListener(async (info, tab) => {
-  const translatedWord = await fetchTranslationBing(info.selectionText.trim());
-  if (translatedWord) {
-    updateMenuItem(translatedWord);
-  } else {
-    updateMenuItem("No Translation found");
-  }
-});
+function equalsIgnoringCase(text, other) {
+  return text.localeCompare(other, undefined, { sensitivity: 'base' }) === 0;
+}
 
-async function fetchTranslationBing(wordToTranslate) {
+async function fetchTranslationBing(wordToTranslate, language) {
   const headers = new Headers();
   headers.append("Content-type", "application/json; charset=UTF-8");
   headers.append("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0");
   const body = `["${wordToTranslate}"]`;
 
-  const request = new Request("https://edge.microsoft.com/translate/translatetext?to=de", {
+  const request = new Request("https://edge.microsoft.com/translate/translatetext?to=" + language, {
     method: "POST",
     headers: headers,
     body: body
