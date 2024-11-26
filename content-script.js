48570a2c7a915d3a37eb2b822253a05461d6dd57
Hannes Oswald <hannes.oswald97@gmail.com>
2024-11-26 20:43:50 +0100

    enable replace with translation in non input fields

--- content-script.js
+++ content-script.js
@@ -1,6 +1,6 @@
 browser.runtime.onMessage.addListener(executeListener);
 
-function executeListener(message){
+function executeListener(message) {
     replaceSelectedText(message);
     browser.runtime.onMessage.removeListener(executeListener);
 }
@@ -10,14 +10,16 @@ function replaceSelectedText(replacementText) {
     const activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
 
     if (
-      (activeElTagName == "textarea") || (activeElTagName == "input" &&
-      /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
-      (typeof activeEl.selectionStart == "number")
+        (activeElTagName == "textarea") || (activeElTagName == "input" &&
+            /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+        (typeof activeEl.selectionStart == "number")
     ) {
         const before = activeEl.value.slice(0, activeEl.selectionStart);
         const after = activeEl.value.slice(activeEl.selectionEnd);
 
         activeEl.value = before + replacementText + after;
+    } else {
+        document.execCommand('insertText', false, replacementText);
     }
 
 }
